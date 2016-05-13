b6f6cae1eb4784a4f2a5480394d93ce450a1486c
Brian Smith <block8437@gmail.com>
2016-05-13 11:10:46 -0400

    Added variable declaration detection in expression statements.

--- src/main/java/infosec/AST/Statement/ExpressionStatement.java
+++ src/main/java/infosec/AST/Statement/ExpressionStatement.java
@@ -1,9 +1,15 @@
 package infosec.AST.Statement;
 
 import infosec.AST.Expression.*;
+import java.util.Arrays;
 
 public class ExpressionStatement extends Statement {
+    private String[] var_dec = new String[] {"=", "+=", "-=", "*=", "/=", "&=", "^="};
     private Expression exp;
+
+    private boolean isVarDec(String op) {
+        return Arrays.asList(var_dec).indexOf(op) > -1;
+    }
 
     public ExpressionStatement(Expression exp) {
         this.exp = exp;
@@ -12,6 +18,9 @@ public class ExpressionStatement extends Statement {
     public String getType() {
         if ( exp instanceof FunctionCallExpression ) {
             return "fncall";
+        }
+        else if ( exp instanceof InfixExpression && isVarDec(((InfixExpression) exp).getOP()) ) {
+            return "varset";
         }
         else {
             return "return";
